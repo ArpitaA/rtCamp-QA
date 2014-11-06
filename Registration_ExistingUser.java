20084d7ea5c687349df691f6f70fb7c70ed6dc1d
ArpitaA <arpita.ajit@gmail.com>
2014-11-06 17:01:07 +0530

    Create Registration_ExistingUser.java

---
+++ Registration_ExistingUser.java
@@ -0,0 +1,23 @@
+import java.awt.AWTException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class Registration_ExistingUser {
+	public static void main(String[] args) throws InterruptedException, AWTException {
+		WebDriver driver=new FirefoxDriver();
+		//for chrome browser
+		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe path");
+		//WebDriver driver=new ChromeDriver();
+		driver.get("http://demo.rtcamp.com/rtmedia/");
+		Thread.sleep(5000);
+		driver.findElement(By.linkText("Register")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("rtAS_registration_username")).sendKeys("test.sel");
+		driver.findElement(By.id("rtAS_registration_email")).click();
+		new WebDriverWait(driver,60).until(ExpectedConditions.visibilityOfElementLocated(By.className("not-available")));
+		System.out.println("User Already Exists");
+}
+}
