db568e99ff1d8dd16f2b27275a99017c59838cb7
ArpitaA <arpita.ajit@gmail.com>
2014-11-06 17:03:00 +0530

    Create PostUpdate.java

---
+++ PostUpdate.java
@@ -0,0 +1,60 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+
+public class PostUpdate {
+	public static void main(String[] args) throws InterruptedException, AWTException {
+		WebDriver driver=new FirefoxDriver();
+		//for chrome browser
+		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe path");
+		//WebDriver driver=new ChromeDriver();
+		driver.get("http://demo.rtcamp.com/rtmedia/");
+		Thread.sleep(5000);
+		Login(driver);
+		PostUpdatePublic(driver);
+	}
+	public static void Login(WebDriver driver) throws InterruptedException
+	{
+		driver.findElement(By.cssSelector(".rtp-login.button.tiny")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.id("bp-login-widget-user-login")).sendKeys("test.sel");
+		driver.findElement(By.id("bp-login-widget-user-pass")).sendKeys("testsel");
+		driver.findElement(By.id("bp-login-widget-submit")).click();
+		new WebDriverWait(driver, 60).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a.rtp-current-user-avatar.dropdown[title='test-sel']")));
+		System.out.println("User logged-in successfully");
+	}
+	public static void PostUpdatePublic(WebDriver driver) throws InterruptedException, AWTException
+	{
+	driver.findElement(By.cssSelector(".rtmedia-add-media-button")).click();
+	Thread.sleep(5000);
+	setClipboardData("C:\\Users\\Public\\Pictures\\Sample Pictures\\Koala.jpg");
+	//native key strokes for CTRL, V and ENTER keys
+	Robot robot = new Robot();
+	robot.keyPress(KeyEvent.VK_CONTROL);
+	robot.keyPress(KeyEvent.VK_V);
+	robot.keyRelease(KeyEvent.VK_V);
+	robot.keyRelease(KeyEvent.VK_CONTROL);
+	robot.keyPress(KeyEvent.VK_ENTER);
+	robot.keyRelease(KeyEvent.VK_ENTER);
+	robot.delay(1000);
+	new WebDriverWait(driver, 60).until(ExpectedConditions.visibilityOfElementLocated(By.id("rtm-upload-start-notice")));
+	System.out.println("File uploaded successfully");
+	driver.findElement(By.id("whats-new")).sendKeys("Test");
+	driver.findElement(By.id("aw-whats-new-submit")).click();
+	new WebDriverWait(driver,60).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li.just-posted")));
+	System.out.println("post updated successfully");
+	}
+	public static void setClipboardData(String string) {
+		   StringSelection stringSelection = new StringSelection(string);
+		   Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+		}
+}
